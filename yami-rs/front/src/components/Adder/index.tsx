1758b5b6882f6911de41c6dd04276ce6d47e4f1c
carrotflakes <carrotflakes@gmail.com>
2024-01-04 23:26:00 +0900

    Refactor Adder component

--- yami-rs/front/src/components/Adder/index.tsx
+++ yami-rs/front/src/components/Adder/index.tsx
@@ -9,30 +9,31 @@ export const Adder = ({ query }: { query?: string }) => {
 
   const handleAdd = useCallback(async () => {
     const p = rawCode.split(/\s+/g)
-    if (p.length === 3) {
-      const a: any[] = p.map(x => {
-        if (x[0] === "$")
-          return +x.slice(1)
-        return parse(x)
-      });
+    if (p.length !== 3)
+      return;
 
-      let code;
-      if (a.find(x => typeof x === "number") !== undefined) {
-        const rows = csv.trim().split('\n').map(x => x.split(',').map(x => x.trim()).filter(x => x !== ''));
-        if (rows.length === 0) return;
-        if (!rows.every(row => row.length === rows[0].length)) return;
-        const parsedRows = rows.map(row => row.map(x => JSON.stringify(x)));
-        code = `(and ${parsedRows.map(row =>
-          `(sym (${a.filter(x => x.type === 'var').map(x => x.rep).filter((x, i, a) => a.indexOf(x) === i).join(' ')})
+    const a: any[] = p.map(x => {
+      if (x[0] === "$")
+        return +x.slice(1)
+      return parse(x)
+    });
+
+    let code;
+    if (a.every(x => typeof x !== "number")) {
+      const rows = csv.trim().split('\n').map(x => x.split(',').map(x => x.trim()).filter(x => x !== ''));
+      if (rows.length === 0) return;
+      if (!rows.every(row => row.length === rows[0].length)) return;
+      const parsedRows = rows.map(row => row.map(x => JSON.stringify(x)));
+      code = `(and ${parsedRows.map(row =>
+        `(sym (${a.filter(x => x.type === 'var').map(x => x.rep).filter((x, i, a) => a.indexOf(x) === i).join(' ')})
         (add ${f(a[0], row)} ${f(a[1], row)} ${f(a[2], row)}))`
-          ).join(' ')})`
-      } else {
-        code = `(sym (${a.filter(x => x.type === 'var').map(x => x.rep).filter((x, i, a) => a.indexOf(x) === i).join(' ')})
+      ).join(' ')})`
+    } else {
+      code = `(sym (${a.filter(x => x.type === 'var').map(x => x.rep).filter((x, i, a) => a.indexOf(x) === i).join(' ')})
         (add ${a[0].rep} ${a[1].rep} ${a[2].rep}))`
-      }
-      const res = await fetcher(API_ENDPOINT, code)
-      setStatus(res + '')
     }
+    const res = await fetcher(API_ENDPOINT, code)
+    setStatus(res + '')
   }, [csv, rawCode])
 
   const handleRemove = useCallback(async () => {
@@ -60,10 +61,12 @@ export const Adder = ({ query }: { query?: string }) => {
   );
 }
 
-function parse(x: string) {
+function parse(x: string): { type: "sym" | "str" | "var", rep: string } {
   if (x[0] === ':')
     return { type: 'sym', rep: x };
-  return x[0] === '"' ? { type: 'str', rep: x } : { type: 'var', rep: x };
+  if (x[0] === '"')
+    return { type: 'str', rep: x };
+  return { type: 'var', rep: x };
 }
 
 function f(x: any, row: any[]) {
